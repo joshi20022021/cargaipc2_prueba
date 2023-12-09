832f03431e52af6defaf8bfa4d90f5aeb3fc3aad
Edgar Josías Cán Ajquejay <113130705+joshi20022021@users.noreply.github.com>
2023-12-08 23:02:20 -0600

    Update main.py

--- IPC2_PROYECTO1/main.py
+++ IPC2_PROYECTO1/main.py
@@ -4,23 +4,21 @@ from tkinter import Tk
 from cancion import Cancion
 from nodo import Nodo
 
-# Configurar la ventana de Tkinter para seleccionar el archivo
 raiz = Tk()
-raiz.withdraw()  # Ocultar la ventana principal
+raiz.withdraw()  
 
-# Pedir al usuario que seleccione un archivo XML
+
 archivo_xml = filedialog.askopenfilename(title="Selecciona un archivo XML", filetypes=[("Archivos XML", "*.xml")])
 
 if archivo_xml:
-    # Leer el archivo XML
+
     tree = ET.parse(archivo_xml)
     root = tree.getroot()
 
-    # Crear una lista doblemente enlazada para almacenar las canciones
     lista_canciones = None
     canciones = root.findall('cancion')
 
-    # Iterar sobre cada elemento 'cancion' en el XML y crear objetos Cancion
+
     for cancion in canciones:
         nombre = cancion.get('nombre')
         artista = cancion.findtext('artista')
@@ -28,7 +26,7 @@ if archivo_xml:
         imagen = cancion.findtext('imagen')
         ruta = cancion.findtext('ruta')
 
-        # Verificar si algún campo está vacío antes de crear el objeto Cancion
+
         if artista is not None and album is not None and imagen is not None and ruta is not None:
             nueva_cancion = Cancion(nombre, artista, album, imagen, ruta)
             
@@ -42,7 +40,7 @@ if archivo_xml:
                 actual.nodo.siguiente = Nodo(nueva_cancion)
                 actual.nodo.siguiente.anterior = actual.nodo
 
-     # Imprimir los datos de la lista doblemente enlazada hacia adelante
+
     actual = lista_canciones
     while actual:
         print(f"Nombre: {actual.nombre}, Artista: {actual.artista}, Album: {actual.album}, Imagen: {actual.imagen}, Ruta: {actual.ruta}")
@@ -52,7 +50,7 @@ if archivo_xml:
         else:
             break
 
-    # Imprimir los datos de la lista doblemente enlazada hacia atrás
+
     ultimo = actual
     while ultimo:
         anterior = ultimo.nodo.anterior
