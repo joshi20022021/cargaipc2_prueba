a32d797e6b6f3f4ef9e585d7177a03dee896414f
Edgar Josías Cán Ajquejay <113130705+joshi20022021@users.noreply.github.com>
2023-12-08 23:02:38 -0600

    Update cancion.py

--- IPC2_PROYECTO1/cancion.py
+++ IPC2_PROYECTO1/cancion.py
@@ -1,7 +1,5 @@
 from nodo import Nodo
 
-# Definición de la clase Cancion que utiliza el nodo para la lista doblemente enlazada
-
 class Cancion:
     def __init__(self, nombre, artista, album, imagen, ruta):
         self.nombre = nombre
